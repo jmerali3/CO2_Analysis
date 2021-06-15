a83e24f7c0c25177a92b0d0276f85f6dbbc99609
Jameel Merali <jameelmerali1992@gmail.com>
2021-06-14 21:42:32 -0700

    Modify docstrings and clean up function CO2_Model
    Converted input arguments to tuples and outputs to dictionary to improve
    code cleanliness

--- CO2_Model.py
+++ CO2_Model.py
@@ -30,26 +30,27 @@ def load_data():
 
 
 def split_data(features, labels, split_frac=.8):
-    """Converts the feature and label Pandas series to numpy then splits into train and test sets"""
+    """Converts the feature (dates) and label (CO2) Pandas series to numpy then splits into train and test sets"""
     split_num = int(split_frac * len(features))
     features = features.to_numpy().reshape(-1, 1)
     labels = labels.to_numpy().reshape(-1, 1)
-    train_f = features[0:split_num]
-    test_f = features[split_num:]
-    train_l = labels[0:split_num]
-    test_l = labels[split_num:]
-    return train_f, test_f, train_l, test_l
+    train_date = features[0:split_num]
+    test_date = features[split_num:]
+    train_y = labels[0:split_num]
+    test_y = labels[split_num:]
+    return train_date, test_date, train_y, test_y
 
 
 def calc_error(y, predicted_y):
     ''' Computes the residuals, root mean squared error, and mean absolute percentage error'''
-    # TODO Add error_dict and return
+    error_dict = {}
     resid = np.zeros([len(y), 1])
     for i, (y_i, yp_i) in enumerate(zip(y, predicted_y)):
         resid[i] = y_i - yp_i
-    rmse = metrics.mean_squared_error(y, predicted_y) ** .5
-    mape = metrics.mean_absolute_percentage_error(y, predicted_y)
-    return resid, rmse, mape * 100
+    error_dict["resid"] = resid
+    error_dict["rmse"] = metrics.mean_squared_error(y, predicted_y) ** .5
+    error_dict["mape"] = 100 * metrics.mean_absolute_percentage_error(y, predicted_y)
+    return error_dict
 
 
 def interpolate1d_function(x, y, x_new):
@@ -58,56 +59,48 @@ def interpolate1d_function(x, y, x_new):
     return f(x_new)
 
 
-def poly_regression(x, y, test_x, test_y=None, degree=2, error=False):
+def linear_regression(x, test_x, y, test_y=None, error=False):
+    '''Performs linear regression fitted on x and y and returns the model parameters and predicted y
+    This will call the error function and return the error_dictionary if error=True
+    '''
+    lr = linear_model.LinearRegression()
+    lr.fit(x, y)
+    predicted_train_y = lr.predict(x)
+    predict_test_y = lr.predict(test_x)
+    linear_dict = {"coef":lr.coef_, "intercept": lr.intercept_,
+                   "predicted_train_y": predicted_train_y, "predicted_test_y": predict_test_y}
+    error_dict = calc_error(test_y, predict_test_y) if error else None
+    return linear_dict, error_dict
+
+
+def poly_regression(x, test_x, y, test_y=None, degree=2, error=False):
     '''Linearizes the values according to the degree of the polynomial and then calls linear regression
     This will call the error function and return the error_dictionary if error=True
     '''
     pr = PolynomialFeatures(degree=degree)
     x_poly = pr.fit_transform(x)
     test_x_poly = pr.fit_transform(test_x)
-    coef, intercept, predicted_y, predict_test_y = linear_regression(x_poly, y, test_x_poly, test_y)
-    # TODO remove if statement and put outputs in a dictionary
-    # error_dict = calc_error(test_y, predict_test_y) if error else None
-    # return coef, intercept, predicted_y, predict_test_y, error_dict
-    if error:
-        resid, rmse, mape = calc_error(test_y, predict_test_y)
-        return coef, intercept, predicted_y, predict_test_y, resid, rmse, mape
-    else:
-        return coef, intercept, predicted_y, predict_test_y
+    poly_dict, _ = linear_regression(x_poly, test_x_poly, y, test_y)
+    error_dict = calc_error(test_y, poly_dict["predicted_test_y"]) if error else None
+    return poly_dict, error_dict
 
 
-def linear_regression(x, y, test_x, test_y=None, error=False):
-    '''Performs linear regression fitted on x and y and returns the model parameters and predicted y
-    This will call the error function and return the error_dictionary if error=True
-    '''
-    lr = linear_model.LinearRegression()
-    lr.fit(x, y)
-    predicted_y = lr.predict(x)
-    predict_test_y = lr.predict(test_x)
-    # TODO remove if statement and put outputs in a dictionary
-    # error_dict = calc_error(test_y, predict_test_y) if error else None
-    # return lr.coef_, lr.intercept_, predicted_y, predict_test_y, error_dict
-    if error:
-        resid, rmse, mape = calc_error(test_y, predict_test_y)
-        return lr.coef_, lr.intercept_, predicted_y, predict_test_y, resid, rmse, mape
-    else:
-        return lr.coef_, lr.intercept_, predicted_y, predict_test_y
-
-
-def plot_data(train_x, train_y, test_x, test_y, pred_y_train, pred_y_test, title, error):
+def plot_data(split_data_tuple, predictions, title):
     '''Plots the training and test ground truth data and the predicted data'''
-    # TODO put input paramters in a dictionary
-    # TODO remove title and error and savefig. Figures arent changing
+    # TODO put input parameters in a tuple
+    train_x, test_x, train_y, test_y = split_data_tuple
+    pred_y_train, pred_y_test = predictions
     fig, ax = plt.subplots()
     ax.scatter(train_x + 1958 + 2 / 12, train_y, s=5, label="Training Data, Raw")
     ax.scatter(test_x + 1958 + 2 / 12, test_y, s=5, label="Test Data, Raw")
     ax.plot(train_x + 1958 + 2 / 12, pred_y_train, label="Predicted Training Data", c="royalblue", linewidth=.65)
     ax.plot(test_x + 1958 + 2 / 12, pred_y_test, label="Predicted Test Data", c="seagreen", linewidth=.65)
-    ax.set_title(title + " - " + error)
+    ax.set_title(title)
     ax.set_xlabel("Year")
     ax.set_ylabel("CO2 (ppm)")
     ax.legend()
-    # plt.savefig("CO2_Plots/" + title.replace(" ", "_") + ".png")
+    save_title = title.split()[0:3]
+    plt.savefig("CO2_Plots/" + "_".join(save_title))
     plt.show()
 
 
@@ -124,7 +117,7 @@ def plot_cyclic(x, y):
     y_interp = interpolate1d_function(x, y, x_interp)
     ax.plot(x_interp, y_interp)
     fig.tight_layout()
-    # plt.savefig("CO2_Plots/" + title.replace(" ", "_") + ".png")
+    plt.savefig(f"CO2_Plots/{title.replace(' ', '_')}.png")
     plt.show()
 
 
@@ -135,54 +128,39 @@ def sub_plot_data(test_x, resid_quadratic, resid_final):
     ax[1].scatter(test_x, resid_final, c="seagreen")
     ax[0].set_title("Residuals After Trend Residual Removal")
     ax[1].set_title("Residuals After Trend & Periodic Residuals Removal")
-    # plt.savefig("CO2_Plots/Residuals.png")
+    plt.savefig("CO2_Plots/Residuals.png")
+    plt.tight_layout()
     plt.show()
 
 
 def main():
     plot = True  # set to False to hide plots
     co2_df = load_data()
-    train_f, test_f, train_l, test_l = split_data(co2_df["Date Index"], co2_df.CO2)
+    split_data_tuple = split_data(co2_df["Date Index"], co2_df.CO2)
+    train_date_index, test_date_index, train_y, test_y = split_data_tuple
     # date = feature (f), co2 = labels (l)
 
     ### Linear Regression ###
-    (
-        coef_linear, intercept_linear,  # Coefficient(s) and y intercept
-        predicted_co2_train_linear, predicted_co2_test_linear,
-        resid_linear, rmse_linear, mape_linear  # Error metrics on test data
-    ) = linear_regression(train_f, train_l, test_f, test_l, error=True)
-    title_linear = "CO2 Linear Regression"
-    error_linear = f"RMSE: {round(rmse_linear, 2)}"
-    plot_data(train_f, train_l, test_f, test_l, predicted_co2_train_linear, predicted_co2_test_linear,
-              title_linear, error_linear) \
-        if plot is True else None
+    linear_dict, linear_error_dict = linear_regression(*split_data_tuple, error=True)
+    predictions_linear = linear_dict["predicted_train_y"], linear_dict["predicted_test_y"]
+    title_linear = f"CO2 Linear Regression - RMSE:{round(linear_error_dict['rmse'],2)}"
+    plot_data(split_data_tuple, predictions_linear, title_linear) if plot is True else None
 
     ### Quadratic Regression ###
-    (
-        coef_quadratic, intercept_quadratic,  # Coefficient(s) and y intercept
-        predicted_co2_train_quadratic, predicted_co2_test_quadratic,
-        resid_quadratic, rmse_quadratic, mape_quadratic  # Error metrics on test data
-    ) = poly_regression(train_f, train_l, test_f, test_l, degree=2, error=True)
-    title_quadratic = "CO2 Quadratic Regression"
-    error_quadratic = f"RMSE: {round(rmse_quadratic, 2)}"
-    plot_data(train_f, train_l, test_f, test_l, predicted_co2_train_quadratic, predicted_co2_test_quadratic,
-              title_quadratic, error_quadratic) \
-        if plot is True else None
+    quadratic_dict, quadratic_error_dict = poly_regression(*split_data_tuple, degree=2, error=True)
+    predictions_quadratic = quadratic_dict["predicted_train_y"], quadratic_dict["predicted_test_y"]
+    title_quadratic = f"CO2 Quadratic Regression - RMSE: {round(quadratic_error_dict['rmse'],2)}"
+    plot_data(split_data_tuple, predictions_quadratic, title_quadratic) if plot is True else None
 
     ### Cubic Regression ###
-    (
-        coef_cubic, intercept_cubic,  # Coefficient(s) and y intercept
-        predicted_co2_train_cubic, predicted_co2_test_cubic,
-        resid_cubic, rmse_cubic, mape_cubic  # Error metrics on test data
-    ) = poly_regression(train_f, train_l, test_f, test_l, degree=3, error=True)
-    title_cubic = "CO2 Cubic Regression"
-    error_cubic = f"RMSE: {round(rmse_cubic, 2)}"
-    plot_data(train_f, train_l, test_f, test_l, predicted_co2_train_cubic, predicted_co2_test_cubic,
-              title_cubic, error_cubic) \
-        if plot is True else None
+    cubic_dict, cubic_error_dict = poly_regression(*split_data_tuple, degree=3, error=True)
+    predictions_cubic = cubic_dict["predicted_train_y"], cubic_dict["predicted_test_y"]
+    title_cubic = f"CO2 Cubic Regression - RMSE: {round(cubic_error_dict['rmse'],2)}"
+    plot_data(split_data_tuple, predictions_cubic, title_cubic) if plot is True else None
 
     ### Fit Periodic Signal ###
-    train_resid_quadratic, _, _ = calc_error(predicted_co2_train_quadratic, train_l)
+    periodic_error_dict = calc_error(quadratic_dict["predicted_train_y"], train_y)
+    train_resid_quadratic = periodic_error_dict["resid"]
     zero_resid = np.zeros([len(co2_df) - len(train_resid_quadratic), 1])
     resid_joined = np.concatenate([-train_resid_quadratic, zero_resid])
     co2_df["resid"] = resid_joined
@@ -197,21 +175,19 @@ def main():
     plot_cyclic(cyclic_avg["Month"], cyclic_avg["Periodic Residual"]) if plot is True else None
 
     ### Final Model - Quadratic & Periodic Signals ###
-    coef = coef_quadratic.reshape(3)
+    coef = quadratic_dict["coef"].reshape(3)
     first_order, second_order = coef[1], coef[2]
     co2_df["predicted_y"] = co2_df["Date Index"] * first_order + \
                             co2_df["Date Index"] ** 2 * second_order + \
-                            intercept_quadratic + \
+                            quadratic_dict["intercept"] + \
                             co2_df["Periodic Residual"]
-    _, _, predicted_co2_train, predicted_co2_test = split_data(co2_df["Date Index"], co2_df["predicted_y"])
-    resid_test_final, rmse_test_final, mape_test_final = calc_error(test_l, predicted_co2_test)
-    title_final = "CO2 Periodic & Quadratic Regression"
-    error_final = f"RMSE: {round(rmse_test_final, 2)}"
-    plot_data(train_f, train_l, test_f, test_l, predicted_co2_train, predicted_co2_test, title_final, error_final) \
-        if plot is True else None
+    final_predictions = split_data(co2_df["Date Index"], co2_df["predicted_y"])
+    final_error_dict = calc_error(test_y, final_predictions[-1])
+    title_final = f"CO2 Complete Model - RMSE: {round(final_error_dict['rmse'], 2)}"
+    plot_data(split_data_tuple, final_predictions[-2:], title_final) if plot is True else None
 
     ### Residual plots from before and after periodicc signal fitting ###
-    sub_plot_data(test_f, resid_quadratic, resid_test_final) if plot is True else None
+    sub_plot_data(test_date_index, quadratic_error_dict["resid"], final_error_dict["resid"]) if plot is True else None
 
 
 if __name__ == "__main__":
--- CO2_Prediction.py
+++ CO2_Prediction.py
@@ -4,29 +4,28 @@ import matplotlib.pyplot as plt
 import CO2_Model
 import itertools
 
-# Train the model over the entire data set using quadratic and periodic regression.
-# Predict CO2 concentration over the next n years
+"""Train the model over the entire data set using quadratic and periodic regression.
+Predict CO2 concentration over the next n years.
+"""
 
 extrapolate_to_date = 2050  # will extrapolate until February of the selected year
 start_year = 1958  # Do not change this variable
+month = 1/12
 
 # Step 1: Load data, create training features and labels (until 2019), and create test features (future dates)
 co2_df = CO2_Model.load_data()
-train_f = co2_df["Date Index"].to_numpy().reshape(-1, 1)
-train_l = co2_df["CO2"].to_numpy().reshape(-1, 1)
-test_f = np.arange(train_f[-1] + 1/12, (extrapolate_to_date - start_year) + 1/12, 1/12).reshape(-1, 1)  # start year is 1958
+train_date_index = co2_df["Date Index"].to_numpy().reshape(-1, 1)
+train_y = co2_df["CO2"].to_numpy().reshape(-1, 1)
+test_date_index = np.arange(train_date_index[-1] + month, (extrapolate_to_date - start_year) + month, month).reshape(-1, 1)
 
 # Step 2: Perform quadratic regression to obtain coefficients & intercept and predicted CO2
-(
-    coef_quadratic, intercept_quadratic,  # Coefficient(s) and y intercept
-    predicted_co2_train_quadratic, predicted_co2_test_quadratic,
-) = CO2_Model.poly_regression(train_f, train_l, test_f, test_y=None, degree=2, error=False)
+regression_dict, _ = CO2_Model.poly_regression(train_date_index, test_date_index, train_y, test_y=None, degree=2)
 
 # Step 3: Calculate the residuals based on training data and labels
-train_resid_quadratic, _, _ = CO2_Model.calc_error(predicted_co2_train_quadratic, train_l)
+train_error_dict = CO2_Model.calc_error(regression_dict["predicted_train_y"], train_y)
 
 # Step 4: Use residuals to calculated monthly averaged periodic trend. Merge back with original df
-co2_df["resid"] = -train_resid_quadratic
+co2_df["resid"] = -train_error_dict["resid"]
 cyclic_avg = (
     co2_df.groupby('Month')
     .mean()["resid"]
@@ -35,7 +34,7 @@ cyclic_avg = (
     .rename(columns={"resid":"Periodic Residual"}))
 
 # Step 5: Create CO2 test dataframe with appropriate values for columns
-co2_test_df = pd.DataFrame(test_f.reshape(-1, ), columns=["Date Index"])
+co2_test_df = pd.DataFrame(test_date_index.reshape(-1, ), columns=["Date Index"])
 co2_test_df["Month"] = [(i + 8) % 12 + 1 for i in range(1, len(co2_test_df) + 1)]  # Creates month labels for co2_test.
 co2_test_df["CO2"] = np.NaN
 co2_test_df["Year"] = [2019] * 3 + \
@@ -49,21 +48,21 @@ co2_df = co2_df.append(co2_test_df, ignore_index=True)
 
 
 # Step 7: Calculate predicted CO2 based on quadratic & periodic regression
-coef = coef_quadratic.reshape(3)
+coef = regression_dict["coef"].reshape(3)
 first_order, second_order = coef[1], coef[2]
 co2_df["Predicted CO2 Concentration"] = co2_df["Date Index"] * first_order + \
                                         co2_df["Date Index"] ** 2 * second_order + \
-                                        intercept_quadratic + \
+                                        regression_dict["intercept"] + \
                                         co2_df["Periodic Residual"]
 
 
 # Step 8: Plot data
 fig, ax = plt.subplots()
-ax.scatter(co2_df["Date Index"] + start_year + 3/12, co2_df["CO2"], s=5, label="Training Data, Raw")
-ax.plot(co2_df["Date Index"] + start_year + 3/12, co2_df["Predicted CO2 Concentration"], label="Predicted Data", c="seagreen", linewidth=.65)
+ax.scatter(co2_df["Date Index"] + start_year + 3*month, co2_df["CO2"], s=5, label="Training Data, Raw")
+ax.plot(co2_df["Date Index"] + start_year + 3*month, co2_df["Predicted CO2 Concentration"], label="Predicted Data", c="seagreen", linewidth=.65)
 ax.set_title(f"Extrapolated CO2 Concentration to {extrapolate_to_date}")
 ax.set_ylabel("CO2 (ppm)")
 ax.set_xticks(np.arange(1950, extrapolate_to_date+10, 10))
 ax.legend()
-# plt.savefig("CO2_Plots/Extrapolated_CO2_Concentration.png")
+plt.savefig("CO2_Plots/Extrapolated_CO2_Concentration.png")
 plt.show()
